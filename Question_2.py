a670182a1e30c5e53a113824d01cdb27fc4ffc15
Kyle T. Hollands <kylehollands@gmail.com>
2020-06-21 13:17:11 -0400

    Added Question_2 for Python functions.

---
+++ Question_2.py
@@ -0,0 +1,25 @@
+#!/bin/env/usr python
+
+"""Write a Python function to sum all the numbers in a list"""
+
+# Method 1
+
+sample_list = [8, 2, 3, 0, 7]
+
+total = 0
+
+for i in sample_list:
+    total = total + i
+
+print(total)
+
+# Method 2
+
+def sum(numbers):
+    total = 0
+    
+    for x in numbers:
+        total += x
+    return total
+
+print(sum((8, 2, 3, 0, 7)))
